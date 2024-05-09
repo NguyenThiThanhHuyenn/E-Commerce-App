91308a180d2dbfea60001117bef5443180334a82
Nguyen Thi Thanh Huyenn <thithanhhuyennguyen002@gmail.com>
2024-05-09 15:16:27 +0700

    update

--- ECommerceApp/components/Stats/Stats.js
+++ ECommerceApp/components/Stats/Stats.js
@@ -3,6 +3,7 @@ import { View, Text, ActivityIndicator, StyleSheet, ScrollView } from 'react-nat
 import API, { endpoints } from '../../configs/API';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import MyContext from '../../configs/MyContext';
+import { Icon } from 'react-native-elements';
 
 const StatsScreen = ({ userToken }) => {
   const [loading, setLoading] = useState(true);
@@ -67,8 +68,15 @@ const StatsScreen = ({ userToken }) => {
             </View>
           ))}
         </View>
+        <View style={{flexDirection:'row'}}>
+        <Icon name='heart' type='ionicon' size={20} color={'red'}/>
         <Text style={styles.highestRevenue}>Highest Revenue Product (Month): {statistics.highest_revenue_product_month.product__product_name}: {statistics.highest_revenue_product_month.total_revenue} VND</Text>
+        </View>
+        <View style={{flexDirection:'row'}}>
+        <Icon name='warning' type='ionicon' size={20} color={'#967d29'}/>
         <Text style={styles.lowestRevenue}>Lowest Revenue Product (Month): {statistics.lowest_revenue_product_month.product__product_name}: {statistics.lowest_revenue_product_month.total_revenue} VND</Text>
+        </View>
+        
         <Text style={styles.heading}>Quarterly Revenue: {statistics.quarterly_revenue} VND</Text>
         <View style={styles.statsContainer}>
           <Text style={styles.subheading}>Quarterly Product Stats:</Text>
@@ -80,8 +88,15 @@ const StatsScreen = ({ userToken }) => {
             </View>
           ))}
         </View>
+        <View style={{flexDirection:'row'}}>
+        <Icon name='heart' type='ionicon' size={20} color={'red'}/>
         <Text style={styles.highestRevenue}>Highest Revenue Product (Quarter): {statistics.highest_revenue_product_quarter.product__product_name}: {statistics.highest_revenue_product_month.total_revenue} VND</Text>
+        </View>
+        
+        <View style={{flexDirection:'row'}}>
+        <Icon name='warning' type='ionicon' size={20} color={'#967d29'}/>
         <Text style={styles.lowestRevenue}>Lowest Revenue Product (Quarter): {statistics.lowest_revenue_product_quarter.product__product_name}: {statistics.lowest_revenue_product_month.total_revenue} VND</Text>
+        </View>
         <Text style={styles.heading}>Yearly Revenue: {statistics.yearly_revenue} VND</Text>
         <View style={styles.statsContainer}>
           <Text style={styles.subheading}>Yearly Product Stats:</Text>
@@ -93,8 +108,15 @@ const StatsScreen = ({ userToken }) => {
             </View>
           ))}
         </View>
+        <View style={{flexDirection:'row'}}>
+        <Icon name='heart' type='ionicon' size={20} color={'red'}/>
         <Text style={styles.highestRevenue}>Highest Revenue Product (Year): {statistics.highest_revenue_product_year.product__product_name}: {statistics.highest_revenue_product_month.total_revenue} VND</Text>
+        </View>
+        <View style={{flexDirection:'row'}}>
+        <Icon name='warning' type='ionicon' size={20} color={'#967d29'}/>
         <Text style={styles.lowestRevenue}>Lowest Revenue Product (Year): {statistics.lowest_revenue_product_year.product__product_name}: {statistics.lowest_revenue_product_month.total_revenue} VND</Text>
+        </View>
+        
       </View>
     </ScrollView>
   );
@@ -114,7 +136,7 @@ const styles = StyleSheet.create({
       fontSize: 20,
       fontWeight: 'bold',
       marginBottom: 10,
-      color: '#333',
+      color: '#e3c256',
     },
     subheading: {
       fontSize: 18,
@@ -142,16 +164,14 @@ const styles = StyleSheet.create({
     },
     highestRevenue: {
       fontSize: 15,
-      fontWeight: '600',
       marginBottom: 5,
       color: 'gray',
       fontStyle:'italic'
     },
     lowestRevenue: {
       fontSize: 15,
-      fontWeight: '600',
       marginBottom: 50,
-      color: 'red',
+      color: 'gray',
       fontStyle:'italic'
     },
     errorText: {
@@ -161,3 +181,4 @@ const styles = StyleSheet.create({
     },
   });
 export default StatsScreen;
+
